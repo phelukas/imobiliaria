fa981eabbbc9f00345b4ed9a02c2193ff85527b8
phelukas <ppedrolukas19@gmail.com>
2021-05-05 22:07:18 -0300

    primeiro commit

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=20, blank=False, null=False, verbose_name='Primeiro nome')
+    last_name = models.CharField(max_length=20, blank=False, null=False, verbose_name='Segundo nome')
+    
+    def __str__(self):
+        return self.email
